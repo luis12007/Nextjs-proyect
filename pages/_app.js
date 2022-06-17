4c13adf90b2c5a961990265a6d5cbca647736c84
Luis Alexander Hernandez Martinez <00129020@uca.edu.sv>
2022-06-16 18:51:42 -0600

    🎉Add:
    uploading full project with next

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../src/App.css'
+
+export default function App({Component, pageProps}){
+
+    return<>
+    <Component {...pageProps}component/>
+    </>
+
+}
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import App from "../Project/App"
+
+export default function Index ({Props}){
+
+    return<>
+    <App/>
+    </>
+}
